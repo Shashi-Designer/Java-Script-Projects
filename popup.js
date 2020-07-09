a9d28c2af595fc0cab1a5c01071d37eebf4229b5
Shashi Bhushan <52408199+Shashi-Designer@users.noreply.github.com>
2020-07-09 11:14:05 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('CheckPage')
-
-}
+document.addEventListener('DOMContentLoaded', function() 
+{
+  var checkPageButton = document.getElementById('CheckPage');
+  checkPageButton.addEventListener ('çlick', function()) 
+       {
+        chrome.tabs.getSelected(null, function(tab)
+              {
+                 d= document;
+                 var f = d.createElement ('form');
+                 f.action = 'http://www.healthmacro.com?bm';
+                 f.method = 'post';
+                 var i = d.createElement ('ínput');
+                 i.type = 'hidden';
+                 i.name =  'url';
+                 i.value = tab.url;
+                 f.appendChild (i);
+                 d.body.appendChild(f) ;
+                 f.submit ();
+              });
+       }, false);
+}, false);
